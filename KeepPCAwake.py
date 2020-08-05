587e72f45717f72492d3a530551e12b5a596fa53
Eric Reed <mrericcoolnessreed@gmail.com>
2020-08-05 15:35:34 -0400

    Fixed busy wait

--- KeepPCAwake.py
+++ KeepPCAwake.py
@@ -1,4 +1,4 @@
-import ctypes
+import ctypes, threading
 
 ES_CONTINUOUS        = 0x80000000
 ES_SYSTEM_REQUIRED   = 0x00000001
@@ -7,11 +7,7 @@ ES_SYSTEM_REQUIRED   = 0x00000001
 # SYSTEM_REQUIRED 'forces the system to be in the working state by resetting the system idle timer.'
 ctypes.windll.kernel32.SetThreadExecutionState(ES_CONTINUOUS | ES_SYSTEM_REQUIRED)
 
-# wait until broken
-while True:
-    # attempt to run the following code
-    try:
-        # try needs some code as an argument, so I just added a zero.
-        0
-    except(KeyboardInterrupt): # catch a Ctrl+C
-        break # stop waiting
+try:
+    threading.Event ().wait () # infinite non-busy-wait
+except KeyboardInterrupt: # catch a Ctrl-C
+    pass # do nothing
